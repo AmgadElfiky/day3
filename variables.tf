8d1a44b0c7c78d069c73159596c7b8ab1371b674
AmgadElfiky <72378500+AmgadElfiky@users.noreply.github.com>
2024-04-21 15:29:32 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-# not sense to but default values here
-variable "aws_vpc" {}
-variable "aws_subnet" {}
-variable "CIDR_block_route" {}
-variable "instance_type" {}
-variable "ami" {}
-variable "AZ" {}
